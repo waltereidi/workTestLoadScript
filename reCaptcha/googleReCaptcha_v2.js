89269bdd85d54f539815ce6b6b1eb827547c1534
walter-eidi <walter-eidi@hotmail.com>
2024-10-24 20:59:29 -0300

    iniciado prototipação

---
+++ reCaptcha/googleReCaptcha_v2.js
@@ -0,0 +1,8 @@
+import ReCaptcha from "./reCaptcha"
+
+export default class googleReCaptcha_v2 extends ReCaptcha
+{
+    constructor(  ){
+        this.super("body" , alert() , alert())
+    }
+}
---
+++ reCaptcha/googleReCaptcha_v3.js
@@ -0,0 +1,9 @@
+
+import ReCaptcha from "./reCaptcha"
+
+export default class googleReCaptcha_v3 extends ReCaptcha
+{
+    constructor(  ){
+        super("body" , alert() , alert())
+    }
+}
